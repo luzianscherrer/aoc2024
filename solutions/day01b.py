0fbd68356fc60e2834b7e32bd3ca855676889bbc
Luzian Scherrer <luzian@scherrer.ch>
2024-12-01 12:04:24 +0100

    cleanup

--- solutions/day01b.py
+++ solutions/day01b.py
@@ -1,5 +1,4 @@
 import numpy as np
 
-x = np.fromfile("day01input.txt", dtype=int, sep=" ")
-x = x.reshape((x.size // 2, 2))
+x = np.loadtxt("day01input.txt", dtype=int)
 print(sum([n * sum(x[:, 1] == n) for n in x[:, 0]]))
