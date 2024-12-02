2824fd44f23163e2c9f8b001313a88cc0cf14ef4
Luzian Scherrer <luzian@scherrer.ch>
2024-12-02 09:49:13 +0100

    day 2

--- solutions/day01.py
+++ solutions/day01.py
@@ -1,4 +1,5 @@
 import numpy as np
 
 x = np.loadtxt("day01input.txt", dtype=int)
+print(sum(np.abs(np.sort(x[:, 0]) - np.sort(x[:, 1]))))
 print(sum([n * sum(x[:, 1] == n) for n in x[:, 0]]))
--- solutions/day01a.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-x = np.loadtxt("day01input.txt", dtype=int)
-print(sum(np.abs(np.sort(x[:, 0]) - np.sort(x[:, 1]))))
