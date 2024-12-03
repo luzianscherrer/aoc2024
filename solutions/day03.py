36f1600464dcc353fe4eb6988b998cfebd128f6d
Luzian Scherrer <luzian@neoos.ch>
2024-12-03 10:15:43 +0100

    cleanup

--- solutions/day03.py
+++ solutions/day03.py
@@ -1,15 +1,15 @@
 import re
 
 res, valid = [0, 0], 1
-with open("day03input.txt") as file:
-    line = file.read()
-    for match in re.findall(r"(do\(\))|(don't\(\))|(mul\((\d+),(\d+)\))", line):
-        if match[0]:
-            valid = 1
-        elif match[1]:
-            valid = 0
-        else:
-            x = int(match[3]) * int(match[4])
-            res[0] += x
-            res[1] += x * valid
+line = open("day03input.txt").read()
+regex = r"(do\(\))|(don't\(\))|(mul\((\d+),(\d+)\))"
+for match in re.findall(regex, line):
+    if match[0]:
+        valid = 1
+    elif match[1]:
+        valid = 0
+    else:
+        x = int(match[3]) * int(match[4])
+        res[0] += x
+        res[1] += x * valid
 print(res)
