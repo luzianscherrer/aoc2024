e61d282a7a02d419d876c0ae5970f186f428bae3
Luzian Scherrer <luzian@neoos.ch>
2024-12-03 10:12:40 +0100

    day 3

--- solutions/day03.py
+++ solutions/day03.py
@@ -1,9 +1,15 @@
 import re
 
-res = [0, 0]
+res, valid = [0, 0], 1
 with open("day03input.txt") as file:
     line = file.read()
-    res[0] = sum(
-        [int(i[0]) * int(i[1]) for i in re.findall(r"mul\((\d+),(\d+)\)", line)]
-    )
+    for match in re.findall(r"(do\(\))|(don't\(\))|(mul\((\d+),(\d+)\))", line):
+        if match[0]:
+            valid = 1
+        elif match[1]:
+            valid = 0
+        else:
+            x = int(match[3]) * int(match[4])
+            res[0] += x
+            res[1] += x * valid
 print(res)
