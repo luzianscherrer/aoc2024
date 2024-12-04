dbbf2752b7e30b651d3e6c4fa0373276c2618167
Luzian Scherrer <luzian@neoos.ch>
2024-12-04 11:19:51 +0100

    cleanup

--- solutions/day04.py
+++ solutions/day04.py
@@ -5,20 +5,19 @@ a = np.genfromtxt("day04input.txt", dtype=str, delimiter=1)
 
 # part 1
 str = ""
+dim = a.shape[0]
 for i in range(4):
-    for j in range(a.shape[0]):
-        str += "".join(a[j, :]) + ","
-    for j in range(-a.shape[0] + 1, a.shape[0]):
-        str += "".join(a.diagonal(j)) + ","
+    str += ",".join(["".join(a[j, :]) for j in range(dim)])
+    str += ",".join(["".join(a.diagonal(j)) for j in range(-dim + 1, dim)])
     a = np.rot90(a)
 print(len(re.findall(r"XMAS", str)))
 
 # part 2
+count = 0
 a = np.pad(a, 1)
-res = 0
 for y in range(1, a.shape[0] - 1):
     for x in range(1, a.shape[1] - 1):
-        res += (
+        count += (
             a[y, x] == "A"
             and (
                 (a[y - 1, x - 1] == "M" and a[y + 1, x + 1] == "S")
@@ -29,4 +28,4 @@ for y in range(1, a.shape[0] - 1):
                 or (a[y - 1, x + 1] == "S" and a[y + 1, x - 1] == "M")
             )
         )
-print(res)
+print(count)
