79cea7855329a68e488abd154b5ac8574fcfe38a
Luzian Scherrer <luzian@scherrer.ch>
2024-12-05 11:31:16 +0100

    day 5

--- solutions/day05.py
+++ solutions/day05.py
@@ -1,6 +1,6 @@
 import re
 
-rules, result = [], []
+rules, result1, result2 = [], [], []
 with open("day05input.txt") as file:
     for line in file:
         data = line.rstrip()
@@ -14,5 +14,23 @@ with open("day05input.txt") as file:
                     break
             if valid:
                 res = data.split(",")
-                result.append(int(res[len(res) // 2]))
-print(sum(result))
+                result1.append(int(res[len(res) // 2]))
+            else:
+                while True:
+                    valid = True
+                    for rule in rules:
+                        match = re.search(
+                            r"(" + rule[1] + r").+(" + rule[0] + r")", data
+                        )
+                        if match:
+                            valid = False
+                            arr = data.split(",")
+                            tmp = arr[match.span(1)[0] // 3]
+                            arr[match.span(1)[0] // 3] = arr[match.span(2)[0] // 3]
+                            arr[match.span(2)[0] // 3] = tmp
+                            data = ",".join(arr)
+                    if valid:
+                        break
+                res = data.split(",")
+                result2.append(int(res[len(res) // 2]))
+print(sum(result1), sum(result2))
