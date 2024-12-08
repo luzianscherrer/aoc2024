f13c694a55da8080ad29b2efbf9544751c481d11
Luzian Scherrer <luzian@scherrer.ch>
2024-12-08 11:15:53 +0100

    day 8

--- solutions/day08.py
+++ solutions/day08.py
@@ -3,12 +3,14 @@ from itertools import combinations
 
 field = np.genfromtxt("day08input.txt", dtype=str, delimiter=1)
 pos = {}
-res = np.zeros(field.shape)
+res = np.zeros(field.shape, dtype=int)
 
 
-def mark(y, x):
+def mark(y, x, val):
     if y >= 0 and x >= 0 and y < field.shape[0] and x < field.shape[1]:
-        res[y, x] = 1
+        res[y, x] |= val
+        return True
+    return False
 
 
 for symbol in np.unique(field):
@@ -19,8 +21,17 @@ for symbol in np.unique(field):
 for symbol in pos:
     for pair in combinations(pos[symbol], 2):
         slope = (pair[0][1] - pair[1][1]) / (pair[0][0] - pair[1][0])
-        dist = pair[1][0] - pair[0][0]
-        mark(pair[0][0] - dist, pair[0][1] - int(dist * slope))
-        mark(pair[1][0] + dist, pair[1][1] + int(dist * slope))
+        odist = dist = pair[1][0] - pair[0][0]
+        val = 1
+        while mark(pair[0][0] - dist, pair[0][1] - int(dist * slope), val):
+            dist += odist
+            val = 2
+        dist = odist
+        val = 1
+        while mark(pair[1][0] + dist, pair[1][1] + int(dist * slope), val):
+            dist += odist
+            val = 2
+        res[pair[0]] |= 2
+        res[pair[1]] |= 2
 
-print(int(np.sum(res)))
+print(np.sum(res & 1), np.sum(res > 0))
