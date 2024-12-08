9f6967fd45a384bca752e2ec7ae58e290c05ea8e
Luzian Scherrer <luzian@scherrer.ch>
2024-12-08 10:47:26 +0100

    day 8

--- solutions/day08.py
+++ solutions/day08.py
@@ -6,7 +6,7 @@ pos = {}
 res = np.zeros(field.shape)
 
 
-def set(y, x):
+def mark(y, x):
     if y >= 0 and x >= 0 and y < field.shape[0] and x < field.shape[1]:
         res[y, x] = 1
 
@@ -20,7 +20,7 @@ for symbol in pos:
     for pair in combinations(pos[symbol], 2):
         slope = (pair[0][1] - pair[1][1]) / (pair[0][0] - pair[1][0])
         dist = pair[1][0] - pair[0][0]
-        set(pair[0][0] - dist, pair[0][1] - int(dist * slope))
-        set(pair[1][0] + dist, pair[1][1] + int(dist * slope))
+        mark(pair[0][0] - dist, pair[0][1] - int(dist * slope))
+        mark(pair[1][0] + dist, pair[1][1] + int(dist * slope))
 
 print(int(np.sum(res)))
