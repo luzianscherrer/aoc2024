ed057145b839490ad291e066f6235c886ad52a64
Luzian Scherrer <luzian@scherrer.ch>
2024-12-14 09:20:46 +0100

    day 14

--- solutions/day14.py
+++ solutions/day14.py
@@ -1,29 +1,48 @@
 import re
 import numpy as np
 
-input = open("day14input.txt").read().split("\n")
-p = np.zeros((len(input), 2))
+np.set_printoptions(threshold=np.inf)
+np.set_printoptions(linewidth=np.inf)
+
+
+def as_string(m, p):
+    v = np.zeros(m)
+    v[p[:, 0], p[:, 1]] = 1
+    return re.sub(r"[ .\[\]]", "", str(v))
+
+
+def part1(m, p):
+    res = np.zeros(4)
+    for pos in p:
+        if pos[0] < m[0] // 2 and pos[1] < m[1] // 2:
+            res[0] += 1
+        elif pos[0] > m[0] // 2 and pos[1] < m[1] // 2:
+            res[1] += 1
+        elif pos[0] < m[0] // 2 and pos[1] > m[1] // 2:
+            res[2] += 1
+        elif pos[0] > m[0] // 2 and pos[1] > m[1] // 2:
+            res[3] += 1
+    print(f"part1: {int(np.prod(res))}")
+
+
+data = open("day14input.txt").read().split("\n")
+p = np.zeros((len(data), 2), dtype=int)
 v = np.zeros_like(p)
 
-for i, line in enumerate(input):
+for i, line in enumerate(data):
     match = re.search(r"p=(\d+),(\d+) v=(-?\d+),(-?\d+)", line)
     p[i] = [int(match.group(2)), int(match.group(1))]
     v[i] = [int(match.group(4)), int(match.group(3))]
 
-m = np.array([p[:, 0].max() + 1, p[:, 1].max() + 1])
-
-for _ in range(100):
+m = np.array([p[:, 0].max() + 1, p[:, 1].max() + 1]).astype(int)
+i = 0
+while True:
+    if i == 100:
+        part1(m, p)
     p = (p + v) % m
-
-res = np.zeros(4)
-for pos in p:
-    if pos[0] < m[0] // 2 and pos[1] < m[1] // 2:
-        res[0] += 1
-    elif pos[0] > m[0] // 2 and pos[1] < m[1] // 2:
-        res[1] += 1
-    elif pos[0] < m[0] // 2 and pos[1] > m[1] // 2:
-        res[2] += 1
-    elif pos[0] > m[0] // 2 and pos[1] > m[1] // 2:
-        res[3] += 1
-
-print(int(np.prod(res)))
+    i += 1
+    disp = as_string(m, p)
+    if "1111111111" in disp:
+        print(disp)
+        print(f"part2: {i}")
+        break
