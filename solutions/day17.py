c5adbaef840d009e94e3d5d4b95cc302c2ef4824
Luzian Scherrer <luzian@neoos.ch>
2024-12-17 13:27:03 +0100

    day 17

--- solutions/day17.py
+++ solutions/day17.py
@@ -3,33 +3,50 @@ import re
 data = open("day17input.txt").read().replace("\n", "")
 match = re.search(r"A: (\d+).+B: (\d+).+C: (\d+).+m: (.+)", data)
 reg_a, reg_b, reg_c = int(match.group(1)), int(match.group(2)), int(match.group(3))
-prg, ip, output = match.group(4).split(","), 0, []
+prg = match.group(4).split(",")
 
 
-def combo(value):
-    return (list(range(4)) + [reg_a, reg_b, reg_c])[value]
+def combo(value, a, b, c):
+    return (list(range(4)) + [a, b, c])[value]
 
 
-while ip < len(prg) - 1:
-    opcode, operand = int(prg[ip]), int(prg[ip + 1])
-    if opcode == 0:
-        reg_a = reg_a // (2 ** combo(operand))
-    elif opcode == 1:
-        reg_b = reg_b ^ operand
-    elif opcode == 2:
-        reg_b = combo(operand) % 8
-    elif opcode == 3:
-        if reg_a != 0:
-            ip = operand
-            continue
-    elif opcode == 4:
-        reg_b = reg_b ^ reg_c
-    elif opcode == 5:
-        output.append(combo(operand) % 8)
-    elif opcode == 6:
-        reg_b = reg_a // (2 ** combo(operand))
-    elif opcode == 7:
-        reg_c = reg_a // (2 ** combo(operand))
-    ip += 2
+def run(a, b, c, prg):
+    i = 0
+    output = []
+    while i < len(prg) - 1:
+        opcode, operand = int(prg[i]), int(prg[i + 1])
+        if opcode == 0:
+            a = a // (2 ** combo(operand, a, b, c))
+        elif opcode == 1:
+            b = b ^ operand
+        elif opcode == 2:
+            b = combo(operand, a, b, c) % 8
+        elif opcode == 3:
+            if a != 0:
+                i = operand
+                continue
+        elif opcode == 4:
+            b = b ^ c
+        elif opcode == 5:
+            output.append(combo(operand, a, b, c) % 8)
+        elif opcode == 6:
+            b = a // (2 ** combo(operand, a, b, c))
+        elif opcode == 7:
+            c = a // (2 ** combo(operand, a, b, c))
+        i += 2
+    return ",".join(map(str, output))
 
-print(",".join(map(str, output)))
+
+print(run(reg_a, reg_b, reg_c, prg))
+
+cand = [0]
+for lng in range(1, len(list(map(int, prg))) + 1):
+    cand_before = cand
+    cand = []
+    for value in cand_before:
+        for offset in range(8):
+            value_a = 8 * value + offset
+            if run(value_a, reg_b, reg_c, prg) == ",".join(prg[-lng:]):
+                cand.append(value_a)
+
+print(min(cand))
