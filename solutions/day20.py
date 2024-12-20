9708495cd290b0cb9d740cb14ebd73572cbfa030
Luzian Scherrer <luzian@neoos.ch>
2024-12-20 14:05:15 +0100

    day 20

--- solutions/day20.py
+++ solutions/day20.py
@@ -24,15 +24,38 @@ for y in range(1, M.shape[0] - 1):
             continue
         connect_pos(y, x)
 
-
 base_length = nx.shortest_path_length(G, start, end)
+dist = 100
 
-count = 0
+count1 = 0
 for i, wall in enumerate(walls):
-    print(f"{i} of {len(walls)}")
+    if i % 100 == 0:
+        print(f"part1 {i/len(walls)*100:.0f}%")
     G_orig = G.copy()
     connect_pos(*wall)
     this_length = nx.shortest_path_length(G, start, end)
-    count += base_length - this_length >= 100
+    count1 += base_length - this_length >= dist
     G = G_orig
-print(count)
+print("part1 done")
+
+shortest_path = nx.shortest_path(G, start, end)
+shortest_path_int = np.array(
+    [list(map(int, node.split(","))) for node in shortest_path]
+)
+
+count2 = 0
+for i in range(len(shortest_path_int)):
+    if i % 100 == 0:
+        print(f"part2 {i/len(shortest_path_int)*100:.0f}%")
+    j = i + dist
+    while j < len(shortest_path_int):
+        l1_dist = np.linalg.norm(
+            shortest_path_int[i] - shortest_path_int[j],
+            ord=1,
+        )
+        if l1_dist <= 20 and j - i - l1_dist >= dist:
+            count2 += 1
+        j += 1
+print("part2 done")
+
+print(f"part1: {count1}, part2: {count2}")
