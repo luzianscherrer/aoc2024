3b57732962e64caac3dfad4fe15849a9bf7651c5
Luzian Scherrer <luzian@scherrer.ch>
2024-12-22 00:22:54 +0100

    day 21

--- solutions/day21.py
+++ solutions/day21.py
@@ -1,12 +1,16 @@
 import networkx as nx
 import numpy as np
+from functools import cache
+
+dirs = {">": [0, 1], "v": [1, 0], "<": [0, -1], "^": [-1, 0]}
+numpad = np.array([["7", "8", "9"], ["4", "5", "6"], ["1", "2", "3"], ["", "0", "A"]])
+dirpad = np.array([["", "^", "A"], ["<", "v", ">"]])
 
 
 def get_paths(keypad, code):
     G = nx.DiGraph()
     keypad = np.pad(keypad, 1, constant_values="")
 
-    dirs = {">": [0, 1], "v": [1, 0], "<": [0, -1], "^": [-1, 0]}
     for y in range(1, keypad.shape[0] - 1):
         for x in range(1, keypad.shape[1] - 1):
             if len(keypad[y, x]):
@@ -17,7 +21,6 @@ def get_paths(keypad, code):
                         )
 
     best_paths = [""]
-    code = "A" + code
     for i in range(len(code) - 1):
         new_paths = []
         paths = list(nx.all_shortest_paths(G, code[i], code[i + 1]))
@@ -30,20 +33,27 @@ def get_paths(keypad, code):
     return best_paths
 
 
-numpad = np.array([["7", "8", "9"], ["4", "5", "6"], ["1", "2", "3"], ["", "0", "A"]])
-dirpad = np.array([["", "^", "A"], ["<", "v", ">"]])
+@cache
+def resolve(code, distance):
+    res = 0
+    code = "A" + code
+    for i in range(1, len(code)):
+        paths = get_paths(dirpad, code[i - 1 : i + 1])
+        if distance == 0:
+            res += min([len(path) for path in paths])
+        else:
+            res += min(resolve(path, distance - 1) for path in paths)
+    return res
+
+
+def run(code, steps):
+    return min([resolve(x, steps) for x in get_paths(numpad, "A" + code)])
 
 
 data = open("day21input.txt").read().split("\n")
-res = 0
+res1, res2 = 0, 0
 for code in data:
-    paths = get_paths(numpad, code)
-    for i in range(2):
-        new_paths = []
-        for path in paths:
-            new_paths += get_paths(dirpad, path)
-        paths = new_paths
+    res1 += int(code.replace("A", "")) * run(code, 1)
+    res2 += int(code.replace("A", "")) * run(code, 24)
 
-    res += int(code.replace("A", "")) * min([len(path) for path in paths])
-
-print(res)
+print(res1, res2)
