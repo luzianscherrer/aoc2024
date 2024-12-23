f4ad2bea58d7d48a5bd8f368d724307fdbc5825f
Luzian Scherrer <luzian@scherrer.ch>
2024-12-23 14:27:08 +0100

    day 23

--- solutions/day23.py
+++ solutions/day23.py
@@ -2,16 +2,17 @@ import networkx as nx
 import itertools
 
 G = nx.Graph()
-
-tnodes = []
 data = open("day23input.txt").read().split("\n")
 for line in data:
-    a, b = line.split("-")
-    G.add_edge(a, b)
+    G.add_edge(line[0:2], line[3:5])
 
 combos = set()
+max_clique = []
 for clique in nx.find_cliques(G):
+    if len(clique) > len(max_clique):
+        max_clique = clique
     for combo in itertools.combinations(clique, 3):
         if len(list(filter(lambda x: x[0] == "t", combo))):
             combos.add(tuple(sorted(combo)))
 print(len(combos))
+print(",".join(sorted(max_clique)))
